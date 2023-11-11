cc90e2a656335555ff487d9b08b3d7a73bfaa7f2
NeutralElement <nicolas.boyardi@usm.cl>
2023-11-11 14:46:23 -0300

    avance 2

--- Codigos.py
+++ Codigos.py
@@ -17,19 +17,28 @@ for (t1,t2) in TablaDeInstancias:
     TamañoUbicacionesPosibles = random.randint(t1[0],t1[1])
     TamañoTiendasYaInstaladas = random.randint(t2[0],t2[1])
 
+
+
+    #Genera las |J| tiendas ya instaladas
+    TiendasYaInstaladas = []
+    for n in range(1, TamañoTiendasYaInstaladas, 1):            
+        (x,y) = (random.randint(226,375), random.randint(226,375))
+        TiendasYaInstaladas.append((x,y))
+    TiendasYaInstaladas.sort
+
+
     #Ahora hay que calcular h_ij, el peso total de x_ij, i.e. de asignar i a j
-    Dist = np.zeros[(600,600)]
+    Dist = np.zeros((600,600))
     i = 1
-    j = 1
     for x in range(1,600,1):
         for y in range(1,600,1):
-            if (abs(x-300.5)>75) & (abs(y-300.5)>75): #i.e., están fuera de la zona gris
-                for (x2) in range (226,375,1):
-                    for (y2) in range (226,375,1):
-                        Dist[i][j] = ((x-x2)**2+(y-y2)**2)**(1/2)
+            if (abs(x-300.5)>=75.5) & (abs(y-300.5)>=75.5): #i.e., están fuera de la zona gris
+                j=1
+                for z in TiendasYaInstaladas:
+                    Dist[i-1][j-1] = ((x-z[0])**2+(y-z[1])**2)**(1/2)
                     j+=1
-                j+=1
         i+=1
     i+=1
 
     MatricesDeCostos.append(Dist)
+    print(MatricesDeCostos)
