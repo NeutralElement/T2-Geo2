e07ec59fc36e96a2cface4db5d12a4218bbb36b2
NeutralElement <nicolas.boyardi@usm.cl>
2023-11-18 20:48:30 -0300

    Instanciador funcional

--- GeneradorMatrices.py
+++ GeneradorMatrices.py
@@ -2,6 +2,10 @@ import os
 import random
 import numpy as np
 
+import time
+start_time = time.time()
+
+
 NombresArchivos = []
 
 for i in range(1,9,1):
@@ -15,10 +19,10 @@ for i in range(1,9,1):
 
 
 
-TablaDeInstancias = [((3,6),(11,20))]#, ((7,11),(11,20)), 
-                    #((12,15),(11,20)), ((12,15),(20,29))] 
-                    #((15,18),(20,29)),((19,23),(20,29)),
-                    #((19,23),(26,35)),((24,28),(26,35))]
+TablaDeInstancias = [((3,6),(11,20)), ((7,11),(11,20)), 
+                    ((12,15),(11,20)), ((12,15),(20,29)) 
+                    ((15,18),(20,29)),((19,23),(20,29)),
+                    ((19,23),(26,35)),((24,28),(26,35))]
 
 CantidadDeInstancias = len(TablaDeInstancias)
 
@@ -50,7 +54,7 @@ for (t1,t2) in TablaDeInstancias:
     #Ahora hay que calcular h_ij, el peso total de x_ij, i.e. de asignar i a j
     Distancia = np.zeros((337500,TamañoTiendasYaInstaladas))
     CostoInstalacion = np.zeros(337500) 
-    EmisionesOperacion = np.zeros((337500,TamañoTiendasYaInstaladas)) 
+    EmisionesOperacion = np.zeros(337500) 
     Capacidad = np.zeros(337500)
     
     #337500 = 600^2-150^2
@@ -58,22 +62,20 @@ for (t1,t2) in TablaDeInstancias:
     i = 1
     for x in range(1,600+1,1):
         for y in range(1,600+1,1):
-            j=1
             if (abs(x-300.5)>=75.5) or (abs(y-300.5)>=75.5): #i.e., están en la zona verde (contiene a la celeste)
+                j=1
                 for z in TiendasYaInstaladas:
-                    if y > 1:
-                        
-                        if (abs(x-300.5)>=200.5) or (abs(y-300.5)>=200.5): #i.e., están en la zona celeste
-                            CostoInstalacion[i-1] = random.randint(1000,1500)
-                        else:                                              #i.e., estamos en la zona verde
-                            CostoInstalacion[i-1] = random.randint(1500,4000)
-                        
-                        Capacidad[i-1] = random.randint(2,TamañoTiendasYaInstaladas//2)
-                    
-                    Distancia[i-1][j-1] = ((x-z[0])**2+(y-z[1])**2)**(1/2)
-                    EmisionesOperacion[i-1][j-1] = random.randint(20,70)
-
+                    Distancia[i-1][j-1] = abs(x-z[0])+abs(y-z[1])
                     j+=1
+                    
+                if (abs(x-300.5)>=200.5) or (abs(y-300.5)>=200.5): #i.e., están en la zona celeste
+                    CostoInstalacion[i-1] = random.randint(1000,1500)
+                else:                                              #i.e., estamos en la zona verde
+                    CostoInstalacion[i-1] = random.randint(1500,4000)
+                
+                Capacidad[i-1] = random.randint(2,TamañoTiendasYaInstaladas//2)
+                EmisionesOperacion[i-1] = random.randint(20,70)                    
+
                 i+=1
     MatricesDeCostosDeInstalacion.append(CostoInstalacion)
     MatricesDeEmisionesOperacionales.append(EmisionesOperacion)
@@ -86,17 +88,38 @@ def escribir_indexados(conjunto, nombre_conjunto):
     archivo_i.write(nombre_conjunto+':=\n')
     for i, valor in enumerate(conjunto, 1):
         archivo_i.write(f'{i} {valor}\n')
-    archivo_i.write(';\n\n')   
+    archivo_i.write(';\n\n')
+
+def escribir_matriz(conjunto, nombre_conjunto, ):
+    largo = len(conjunto[0])
+
+    archivo_i.write(nombre_conjunto+':\n')
+    for j in range(1,largo+1, 1):
+        if j==1:
+            archivo_i.write("\t")
+        archivo_i.write(str(j)+"\t")
+    archivo_i.write("\n")
+
+    for i, valor in enumerate(conjunto, 1):
+        archivo_i.write(f"{i}")
+        for j in range(1,largo+1,1):
+            archivo_i.write(f'\t{valor[j-1]}')
+        archivo_i.write("\n")
+    archivo_i.write(';\n\n')
+
 
 for i in range (1,CantidadDeInstancias+1,1):
     archivo_i = open(NombresArchivos[i-1], "w")
     
     archivo_i.write("param cardI := "+str(ListaIJ[i-1][0])+";\n")
     archivo_i.write("param cardJ := "+str(ListaIJ[i-1][1])+";\n\n")
-
+    
     escribir_indexados(MatricesDeCostosDeInstalacion[i-1], "param CI")
-    escribir_indexados(MatricesDeDistancias[i-1], "param d")
+    
+    escribir_matriz(MatricesDeDistancias[i-1], "param d")
+    
     escribir_indexados(MatricesDeEmisionesOperacionales[i-1], "param O")
     escribir_indexados(MatricesDeCapacidades[i-1], "param C" )
 
     archivo_i.close()
+print(" %s segundos ---" % (time.time() - start_time))
